2ffb8a0830f49a7e08d2fab28a701cb97eb90bf7
Wesley <email.wesley.tam@gmail.com>
2024-06-20 23:27:52 -0400

    Created graphs of all predictors against each other and against next_hit to get an idea of the collinearity problem as well as the general shortcomings of model

--- ols_model.py
+++ ols_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from sklearn.multioutput import MultiOutputRegressor
 from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 
 
 
@@ -71,9 +73,56 @@ def fit_ols_model():
     print(r_square)
     print(pred)
 
+def ols_model_fit_info():
+    df = pd.read_csv('batter_training_set.csv')
+
+    Y = pd.DataFrame({
+        'next_pa': df['next_pa'],
+        'next_hit': df['next_hit'],
+        'next_single': df['next_single'],
+        'next_double': df['next_double'],
+        'next_triple': df['next_triple'],
+        'next_home_run': df['next_home_run']
+    })
+    X = df.drop(['next_pa', 'next_hit', 'next_single', 'next_double', 'next_triple', 'next_home_run', 'last_name', 'first_name', 'player_id', 'year'], axis = 1)
+    X['intercept'] = np.ones(df['year'].shape)
+    for col in Y:
+        model = sm.OLS(Y[col], X)
+        results = model.fit()
+        print(results.summary())
+def plot():
+    df = pd.read_csv('batter_training_set.csv')
+    X = df.drop(['next_pa', 'next_hit', 'next_single', 'next_double', 'next_triple', 'next_home_run', 'last_name', 'first_name', 'player_id', 'year'], axis = 1)
+
+    fig, ax = plt.subplots(len(X.columns) + 1, len(X.columns) + 1,)
+    plt.tick_params(axis='both', left=False, top=False, right=False, bottom=False, labelleft=False, labeltop=False, labelright=False, labelbottom=False)
+
+    for i in range(0, len(X.columns) + 1):
+        for j in range(0, len(X.columns) + 1):
+            if i == j:
+                if i < len(X.columns):
+                    ax[i][j].text(0.5, 0.5, s = X.columns[i], horizontalalignment='center', verticalalignment='center', fontsize=6)
+                else:
+                    ax[i][j].text(0.5, 0.5, s = 'next_hit', horizontalalignment='center', verticalalignment='center', fontsize=6)
+            else:
+                if i < len(X.columns) and j < len(X.columns):
+                    ax[i][j].scatter(X[X.columns[j]], X[X.columns[i]], s = 0.25)
+                elif i < len(X.columns):
+                    ax[i][j].scatter(df['next_hit'], X[X.columns[i]], s = 0.25)
+                elif j < len(X.columns):
+                    ax[i][j].scatter(X[X.columns[j]], df['next_hit'], s = 0.25)
+            ax[i][j].set_xticklabels([])
+            ax[i][j].set_yticklabels([])
+
+
+
+    
+    plt.show()
 
 
 # createTrainingSets()
-fit_ols_model()
+# fit_ols_model()
+# ols_model_fit_info()
+plot()
 
 
